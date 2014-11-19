4e3fcb383fe00b4d330c3393c7b968a876ee9205
Antonio Tosti <atost001@hammer.cs.ucr.edu>
2014-11-18 20:56:59 -0800

    Initial modification to exec.cpp, difficulty with argv[]

--- src/exec.cpp
+++ src/exec.cpp
@@ -9,101 +9,170 @@
 #include <fcntl.h>
 
 using namespace std;
+/* Broken - Fix later to help clean up code
+void tokenize(char* arr[], char* token, string delims){
+    unsigned cnt = 0;
+    while(token != NULL){
+	cout << "arr[" << cnt << "]: " << arr[cnt] << endl;
+	arr[cnt] = token;
+	token = strtok(NULL, delims);
+	cnt++;
+    }
+    strcat(arr[cnt - 1], "\0");
+    arr[cnt] = token;
+    return;
+}*/
 
 int main(int argc, char* argv[]){
-    bool isOn = true; //Keeps track of whether rshell is still running
     string usrInput; //Holds user input
-    while(isOn){
+    while(1){
         cout << "$ "; //Prints prompt
 	getline(cin, usrInput);
 	char* inputCpy = new char[usrInput.length() + 1]; //pointer for input str
 	strcpy(inputCpy, usrInput.c_str()); //inputCpy holds c-str copy of input
 	unsigned cnt = 0; //counter for slots in argv[]
-	char buf[usrInput.length() + 1];
+	//char buf[usrInput.length() + 1];
 	char* token = strtok(inputCpy, ";"); //removes semicolons
 	char* a[usrInput.length() + 1]; //Creates array for cmds + args
 	while(token != NULL){ //Tokenizes semicolons
-	    cout << "Token: " << token << endl; //Remove later - Fixme
 	    a[cnt] = token;
 	    token = strtok(NULL, ";");
 	    cnt++;
 	}
 	strcat(a[cnt - 1], "\0");
 	a[cnt] = token; //Null terminates a[]
-	int pid = fork(); //Forks the process
-	if(pid == -1){
-	    perror("fork() failed");
-	    exit(1);
-	}else if(pid == 0){ //Child process
-	    int curr = 0;
-	    int pid2 = 1;
-	    for(int i = 0; i < cnt; i++){//Tokenizes a[] removing " "
-		token = strtok(a[i], " "); //Resets token
-		curr = 0;
-		pid = 1;
-		int fdi;
-		int fdo;
-		bool isOutput = false;
-		//char* in = new char[usrInput.length() + 1];
-		//char* out = new char[usrInput.length() + 1];
 
-		while(token != NULL){ //Remove spaces
-		    argv[curr] = token;
-		    //_____Operator Checks - IO Redirection_____
-		    if(isOutput){
-			cout << "argv[curr + 1]: " << argv[curr] << endl;
-			//if((fdi = open(argv[curr + 2]
-			isOutput = false; //Prevents mult. calls
-		    }
-		    if(strcmp(argv[curr], ">") == 0){//Output
-			    //Check that ">" isn't first arg
-			    //Check that it is surrounded by args 'A > B'
-			isOutput = true;
-			cout << "argv[curr - 1]: " << argv[curr - 1] << endl;
+/*
+	int x = 0;
+	for(unsigned in = 0; in < cnt; in++){
+	    cout << "a[" << in << "]: " << a[in] << endl;
+	    x = in;
+	}
+
+//Old Fixme code - delete whenever checking for null in argv[] works
+	cout << "Test:" << endl;
+	cout << a[x+1] << endl;
+	
+	//cout << strcmp(a[x+1], "\0") << endl;
+	cout << "......." << endl;
+	return 0;
+*/
+
+	int curr = 0;
+	for(int i = 0; i < cnt; i++){//Tokenizes a[] removing " "
+	    token = strtok(a[i], " "); //Resets token
+	    curr = 0;
+	    while(token != NULL){ //Remove spaces
+	        argv[curr] = token; //argv[] = a[] (without spaces)	  
+		if(strcmp(argv[curr], "exit") == 0){//Exit check
+		    cout << "Exiting..." << endl;
+		    delete inputCpy;
+		    exit(0);
+		}
+		//_____Operator Checks - IO Redirection_____
 			//Note: curr - 1 is just the prior arg, you need to
 			//couple it with the flags/command it's passed with
 			//and redirect that OUTPUT to your output.txt
-			
+			//--------------------------------------------------
 			//SOLUTION: Have an int "sym" that holds the position
 			//of the last found symbol and then do a for loop
 			//that combines all of the argv[]'s on one end
 			//together and all of the other end together as well
 			//and then update sym each time an operator is found
 			//be sure to iterate it after the for loop concat, tho
-			cout << "argv[curr]: " << argv[curr] << endl;
-		/*
-			close(1);
-			close(0);
-			if((dup(fdo) == -1)){//
-			    perror("dup(fdo) failed\n");//Error check
+		strcat(argv[curr], "\0"); //Null terms
+		token = strtok(NULL, " ");
+		curr++; 
+/*
+	    	int x = 0; //Used for traversing
+	   	int y = 0; //argv[]'s strings' chars
+		int lastSym = 0; //char location of last symbol in an argv[] string
+		while(strcmp(argv[x], "\0") != 0){//for each
+		    while(argv[x][y] != '\0'){// char in argv[]
+			if(argv[x][y] == '>'){//is Output
+			    //if count + 1 == ">", then is Append
+			    lastSym = y;
+			}else if(argv[x][y] == '<'){//is Input
+			    lastSym = y;
+			}else if(argv[x][y] == '|'){//is Pipe
+			    lastSym = y;
 			}
-			if((dup(fdi) == -1)){
-			    perror("dupe(fdi) failed\n");//Error check
+			y++;//iterate through chars
+		    }
+		    //End of a string
+		    if(y > 0){//if there is a symbol in argv[x]
+			int pid2;
+			if((pid2 = fork()) == -1){
+			    perror("pid2() fork failed");
+			    exit(1);
 			}
-		*/
 		    }
-		    strcat(argv[curr], "\0");
-		    curr++; //Fixme - check this is not OORange
-		    token = strtok(NULL, " ");
-	        }
-	        strcat(argv[curr], "\0"); //Null terms the last string
-	        argv[curr] = token; //Null terms the array
-
-		pid2 = fork();
-		if(pid2 == 0){ //Child process
-		    if(execvp(argv[0], argv) == -1){//Runs on each argv[]
-			perror("execvp() failed");
-			exit(1);
-		    }
+		    x++;//iterate through strings
+		    y = 0; //Resets y to beginning of string
 		}
-		//Parent function
-		if(-1 == wait(0)){ //waits on children - execs in order
-		    perror("wait() failed");
+*/
+	    }
+	    strcpy(argv[curr], "\0");
+	    //strcat(argv[curr], "\0"); //Null terms the last string
+	    //argv[curr] = token; //Null terms array
+	    int x = 0; //Used for traversing
+	    int y = 0; //argv[]'s strings' chars
+	    int lastSym = 0; //char location of last symbol in an argv[] string
+	cout << "HALP" << endl;
+		while(argv[x][0] != '\0'){//for each - FIXME - WHY THE HELL DOES THIS NOT TRIGGER?!
+		    while(argv[x][y] != '\0'){// char in argv[]
+			if(argv[x][y] == '>'){//is Output
+			    //if count + 1 == ">", then is Append
+			    lastSym = y;
+			}else if(argv[x][y] == '<'){//is Input
+			    lastSym = y;
+			}else if(argv[x][y] == '|'){//is Pipe
+			    lastSym = y;
+			}
+			cout << "char" << y << ": " << argv[x][y] << endl;
+			y++;//iterate through chars
+		    }
+		    cout << "string" << x << ": " << argv[x] << endl;
+		    cout << "strcmp-argv[" << x++ << "]: " << strcmp(argv[x], "\0") << endl;
+		    //End of a string
+		    /*if(y > 0){//if there is a symbol in argv[x]
+			int pid2;
+			if((pid2 = fork()) == -1){
+			    perror("pid2() fork failed");
+			    exit(1);
+			}
+			
+		    }*/
+		    x++;//iterate through strings
+		    y = 0; //Resets y to beginning of string
+		}
+		cout << "Final argv[" << x << "]: " << argv[x] << endl;	
+
+	    //strcat(argv[curr], "\0"); //Null terms the last string
+	    //argv[curr] = token; //Null terms array
+	    //for(int z = 0; z < curr; z++){
+	//	cout << "argv[" << z << "]: " << argv[z] << endl;
+	  //  }
+	    
+
+	    int pid = fork();
+	    if(pid == -1){
+		perror("pid fork  failed");
+		exit(1);
+	    }
+	    if(pid == 0){ //Child process
+		if(execvp(argv[0], argv) == -1){//Runs on each argv[]
+		    perror("execvp() failed");
 		    exit(1);
 		}
 	    }
+	    //Parent function
+	    if(-1 == wait(0)){ //waits on children - execs in order
+		perror("wait() failed");
+		exit(1);
+	    }
 	}
-    delete inputCpy; //Deallocates memory - Fixme, might go elsewhere
+        delete inputCpy; //Deallocates memory - Fixme, might go elsewhere
     }//End while loop
 
     return 0;

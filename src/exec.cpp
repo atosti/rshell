8cf3a2bb1c4e806454a4172a5df692a828121b6e
Antonio Tosti <atost001@hammer.cs.ucr.edu>
2014-11-19 16:33:36 -0800

    Added Output redirection, error with forking

--- src/exec.cpp
+++ src/exec.cpp
@@ -23,6 +23,69 @@ void tokenize(char* arr[], char* token, string delims){
     return;
 }*/
 
+void redirect(char* argv[], int num){//consider making a bool
+    int x = 0;
+    int y = 0;
+    int last = -1;//char loc. of last symbol
+    int flags = -1; //Holds val corresponding to type of sym
+
+    while(x != num){//for each string
+	//cout << argv[x] << endl; 
+        y = 0;//Resets y to beginning of a string
+	while(argv[x][y] != '\0'){// char in argv[]
+	    if(argv[x][y] == '>'){//is Output
+		if(argv[x][y+1] == '>'){//Append
+		    //cout << "Append found!" << endl;
+		    flags = 1;
+		}else{
+		    //cout << "Output found!" << endl;
+		    last = x;
+		    flags = 0;
+		}
+	    }else if(argv[x][y] == '<'){//is Input
+		//cout << "Input found!" << endl;
+		flags = 2;
+	    }else if(argv[x][y] == '|'){//is Pipe
+		//cout << "Pipe found!" << endl;
+		flags = 3;
+	    }
+	    //cout << "char" << y << ": " << argv[x][y] << endl;
+	    y++;//iterate through chars
+	}//End of a string
+	//cout << "string" << x << ": " << argv[x] << endl;
+	x++;//iterate through strings
+    }
+    //cout << "Flags before check: " << flags << endl;
+    if(flags == 0){//Output
+	if(last == (x - 1)){//Ends with >
+	    cout << "error: expected argument for '>'" << endl; 
+	    //FIXME - Ensure exit goes back to prompt properly
+	    exit(0);
+	}else if(last == 0){//Symbol is first cmd
+	    //FIXME - need to fdo open then cout will overwrite output file
+	    cout << "";//Clears the output file
+	}else{
+	    //cout << "argv[x-1]: " << argv[x-1] << endl;
+	    int fdo = open(argv[x - 1], O_WRONLY);
+	    if(fdo == -1){
+		perror("open output");
+		exit(0);
+		//Go back to prompt
+	        //ensure execvp is NOT run on this
+	    }
+	    //cout << "Modding fd's..." << endl;
+	    close(1);//close stdout
+	    dup(fdo); //fdo is now in slot 1(stdout)
+	}
+    }
+    //cout << "After mods, last: " << last << endl;
+    if(flags != -1){//Recursive call
+	redirect(argv, last);
+    }
+    return;
+
+}
+
 int main(int argc, char* argv[]){
     string usrInput; //Holds user input
     while(1){
@@ -58,72 +121,42 @@ int main(int argc, char* argv[]){
 		token = strtok(NULL, " ");
 		curr++; 
 	    }
-	    //strcat(argv[curr], "\0"); //Null terms the last string
 	    argv[curr] = token; //Null terms array
-	   /* if(argv[curr] == NULL){
-	        cout << "argv[" << curr << "] is NULL" << endl;
-	      }*/
-
-	    //Start of I/O Redirection
-	    int x = 0; //Used for traversing
-	    int y = 0; //argv[]'s strings' chars
-	    int lSym = 0;//char location of last symbol in an argv[] string
-	    int fSym = 0;//char loc of first symbol
-	    bool isFirst = true; //if first symbol
-	    while(argv[x] != NULL){//for each string
-		y = 0;//Resets y to beginning of a string
-		isFirst = true;
-		while(argv[x][y] != '\0'){// char in argv[]
-		    if(argv[x][y] == '>'){//is Output
-			cout << "Output found!" << endl;
-			//if count + 1 == ">", then is Append
-			if(isFirst){
-			    fSym = y;
-			    isFirst = false;
-			}
-
-			lSym = y;
-		    }else if(argv[x][y] == '<'){//is Input
-			cout << "Input found!" << endl;
-			if(isFirst){
-			    fSym = y;
-			    isFirst = false;
-			}
-			lSym = y;
-		    }else if(argv[x][y] == '|'){//is Pipe
-			cout << "Pipe found!" << endl;
-			if(isFirst){
-			    fSym = y;
-			    isFirst = false;
-			}
-			lSym = y;
-		    }
-		    cout << "char" << y << ": " << argv[x][y] << endl;
-		    y++;//iterate through chars
-		}//End of a string
-		cout << "string" << x << ": " << argv[x] << endl;
-		x++;//iterate through strings
-	    }
-	    //cout << "Final argv[" << x << "]: " << argv[x] << endl;	
-
-	    //strcat(argv[curr], "\0"); //Null terms the last string
-	    //argv[curr] = token; //Null terms array
-
-	    int pid = fork();
-	    if(pid == -1){
-		perror("pid fork  failed");
+	    int pid2 = fork(); //Forks before redirect()
+	    if(pid2 == -1){
+		perror("pid2 failed");
 		exit(1);
-	    }
-	    if(pid == 0){ //Child process
-		if(execvp(argv[0], argv) == -1){//Runs on each argv[]
-		    perror("execvp() failed");
+	    }else if(pid2 == 0){//Child process 
+	        int num = 0;
+	    	while(argv[num] != NULL){//counts length of ars in argv
+		    num++;
+	        }
+		cout << "Num: " << num << endl;
+	        redirect(argv, num); //Calls check for i/o redirection
+		//Create new arr[] here?
+
+	        int pid3 = fork();
+	        if(pid3 == -1){
+		    perror("pid fork  failed");
+		    exit(1);
+	        }
+	  	if(pid3 == 0){ //Child process
+	 	    if(execvp(argv[0], argv) == -1){//Runs on each argv[]
+		    	perror("execvp() failed");
+		        exit(1);
+		    }
+	    	}
+	   	 //Parent function
+	   	 if(-1 == wait(0)){ //waits on children - execs in order
+		    perror("wait() failed");
+		    exit(1);
+	        }
+	    //end pid2 process
+	    }else{//pid2 parent
+		if(-1 == wait(0)){
+		    perror("wait() failed");
 		    exit(1);
 		}
-	    }
-	    //Parent function
-	    if(-1 == wait(0)){ //waits on children - execs in order
-		perror("wait() failed");
-		exit(1);
 	    }
 	}
         delete inputCpy; //Deallocates memory - Fixme, might go elsewhere
